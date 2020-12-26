78459ba9be9af8199ce56a9d0d585bc733172314
Hosoe <juice0524@gmail.com>
2020-12-27 01:14:53 +0900

    environ

--- notice.py
+++ notice.py
@@ -1,9 +1,10 @@
 import requests
 import json
+import os
 
 # WEB_HOOK_URLは下準備で発行したURLを設定しください
-WEB_HOOK_URL = "https://hooks.slack.com/services/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"
+WEB_HOOK_URL = os.environ["SLACK_URL"]
 
 requests.post(WEB_HOOK_URL, data=json.dumps({
     "text" : "送信テスト",
-}))
+}))git
